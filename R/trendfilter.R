6d1cfb4d029e8d44c30cca2cb6b7e1cd8a5591a3
Daniel J. McDonald <dajmcdon@gmail.com>
2023-08-17 16:24:40 -0700

    fix bolixed namespace

--- R/trendfilter.R
+++ R/trendfilter.R
@@ -1,7 +1,11 @@
-
-#' @useDynLib trendfilter .registration = TRUE
+## usethis namespace: start
+#' @useDynLib trendfilter, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @import RcppEigen
+## usethis namespace: end
+NULL
+
+
 #' @import Matrix
 #' @import dspline
 NULL
